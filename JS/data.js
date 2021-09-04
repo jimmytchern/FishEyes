e8d1a48380188d3a786ef33304e557bd644542af
TchernenskyJimmy <tchernensky.jimmy@hotmail.fr>
2021-09-04 18:18:35 +0200

    deleted files useless

--- JS/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// fetch méthode
-fetch("./JS/data.json")
-  //transformer le fichier json
-  .then((response) => response.json())
-  //le log dans la console
-  .then((data) => console.log(data.photographers));
